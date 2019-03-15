b93d841b029c0d28cfe01187cf5fec3cf671c9c7
Siu <kungsiuchun@gmailcom>
2019-03-14 19:30:48 -0700

    Initial Commit

---
+++ CityWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityWeatherApp
+//
+//  Created by SiuChun Kung on 3/14/19.
+//  Copyright © 2019 SiuChun Kung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
